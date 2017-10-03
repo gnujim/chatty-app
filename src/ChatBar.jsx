ad1693a99f10d644d6e72254be0539c5444878d1
Mijung Lee <Mijung@Stephens-MacBook-Air.local>
2017-10-03 13:31:41 -0700

    implemented onMessageChange and onSubmitMessage functions

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 
 class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chatMessage: ''
+    }
+    this.onMessageChange = this.onMessageChange.bind(this);
+    this.onSubmitMessage = this.onSubmitMessage.bind(this);
+  }
+
+  // onMessageChange function listens for changes in input
+  // and sets chatMessage state to current input value
+  onMessageChange(event) {
+    this.setState({
+      chatMessage: event.target.value
+    })
+  }
+
+  // onSubmitMessage function listens for enter keypress and
+  // invokes addMessage function from APP to add msg to MessageList
+  onSubmitMessage(event) {
+    if (event.key == 'Enter'){
+      this.props.addMessage(this.state.chatMessage);
+      this.setState({
+        chatMessage: ''
+      })
+    }
+  }
+
   render() {
-    console.log('Rendering <ChatBar/>');
     return (
       <footer className="chatbar">
         <input
-          defaultValue={this.props.currentUser}
           className="chatbar-username"
           placeholder="Your Name (Optional)"
+          defaultValue={ this.props.currentUser }
         />
         <input
           className="chatbar-message"
           placeholder="Type a message and hit ENTER"
+          onInput={ this.onMessageChange }
+          onKeyPress={ this.onSubmitMessage }
+          value={ this.state.chatMessage }
         />
       </footer>
     );
